5e9f44a28bf2b48b1b91f4cd8ee6fd7178e6c014
jitendra <jitendra.jjr@gmail.com>
2017-02-06 17:41:52 +0100

    Controller class

--- SpringMvc/src/main/java/com/springMvc/boxapp/controller/HomeController.java
+++ SpringMvc/src/main/java/com/springMvc/boxapp/controller/HomeController.java
@@ -7,42 +7,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springMvc.boxapp.dao.BoxDao;
 import com.springMvc.boxapp.model.Box;
 
-@Controller
+@RestController
+
 public class HomeController {
 	@Autowired
 	private BoxDao boxDao;
 
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
-		return new ModelAndView("home");
-		
-	}
-	@RequestMapping(value="/addbox",method = RequestMethod.POST)
-	public ModelAndView addBox(@ModelAttribute Box box){
-		boxDao.addBook(box);
-		return new ModelAndView("redirect:/");
+		return new ModelAndView("home");	
 	}
 	
-	@RequestMapping(value="/listboxes")
-	public ModelAndView listBoxes(ModelAndView model) throws IOException{
+	
+	@RequestMapping(value="/addbox",method = RequestMethod.POST,consumes="application/json")
+	@ResponseBody public Box addBox(@RequestBody Box box){
+		boxDao.addBook(box);
+		return box;
+	}
+	
+	@RequestMapping(value="/listboxes", method=RequestMethod.GET)
+	public @ResponseBody List<Box> listBoxes() {
 	    List<Box> listBoxes =boxDao.listBoxes();
-	    model.addObject("listBoxes", listBoxes);
-	    model.setViewName("home");
-	    return model;
+	    return listBoxes;
 	}
-	@RequestMapping(value = "/deletebox", method = RequestMethod.GET)
-	public ModelAndView deleteBox(HttpServletRequest request) {
-	    int boxId = Integer.parseInt(request.getParameter("id"));
-	    boxDao.deleteBox(boxId);
-	    return new ModelAndView("redirect:/home.jsp");
-	}
+	
 }
